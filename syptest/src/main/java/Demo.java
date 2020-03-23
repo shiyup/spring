c0389159c761d1a8a3eaa6d708787a163ce89e7c
shiyup <172654682@qq.com>
2020-03-23 12:23:42 +0800

    Default Changelist

---
+++ syptest/src/main/java/Demo.java
@@ -0,0 +1,8 @@
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * created by shiyuping on 2020/3/1
+ */
+@Configuration
+public class Demo {
+}
---
+++ syptest/src/main/java/Server.java
@@ -0,0 +1,10 @@
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * created by shiyuping on 2020/3/1
+ */
+public class Server {
+	public static void main(String[] args){
+	   new AnnotationConfigApplicationContext();
+	}
+}
